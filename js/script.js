526d056cf67d895f8281d8279b0f52f2730ee494
Alejo Martinez <brayanm531@gmail.com>
2026-07-15 21:37:48 -0500

    Enhance tip calculator UI and functionality: add styles, improve layout, and refine JavaScript logic for better user experience

--- js/script.js
+++ js/script.js
@@ -1,31 +1,99 @@
-const cuentaTotal = document.querySelector('#cuenta')
-const porcentaje = document.querySelector('#porcentaje')
-const cantidadPersonas = document.querySelector('#personas')
+const inputValorTotal = document.querySelector('#cuenta')
+const inputPorcentaje = document.querySelector('#porcentaje')
+const inputCantidadPersonas = document.querySelector('#personas')
 
-const resultadoSubtotal = document.querySelector('#resultado-subtotal')
-const cantidadPropina = document.querySelector('#resultado-propina')
-const resultadoTotal = document.querySelector('#resultado-total')
-const resultadoPorPersona = document.querySelector('#resultado-por-persona')
+const spanSubtotal = document.querySelector('#resultado-subtotal')
+const spanPropina = document.querySelector('#propina-total')
+const spanResultadoPropina = document.querySelector('#resultado-propina')
+const spanTotalPersonas = document.querySelector('#resultado-por-persona')
+const spanResultadoTotal = document.querySelector('#resultado-total')
 
-function calcularValores() {
-    const valorTotal = parseFloat(cuentaTotal.value)
-    const valorPorcentaje = parseInt(porcentaje.value)
-    const valorPersonas = parseInt(cantidadPersonas.value)
-    
-    if (valorTotal < 0 || valorPorcentaje < 0 || valorPersonas <= 0){
-        console.error ('Ingrese un dato adecuado')
-        return
-    }
+const bloqueMensajeAlerta = document.querySelector('.alert-message')
 
-    const propinaFinal = valorTotal * (valorPorcentaje / 100)
-    const precioFinal = (valorTotal + propinaFinal)
-
-    resultadoSubtotal.textContent = valorTotal
-    cantidadPropina.textContent = propinaFinal
-    resultadoTotal.textContent = precioFinal
-    resultadoPorPersona.textContent = precioFinal / valorPersonas
+function convertirValoresEnteros(valor) {
+    return parseInt(valor.value)
 }
 
-cuentaTotal.addEventListener('input', calcularValores)
-porcentaje.addEventListener('input', calcularValores)
-cantidadPersonas.addEventListener('input', calcularValores)
+function convertirValoresDecimales(valor) {
+    return parseFloat(valor.value)
+}
+
+function actualizarContenido(span, contenido) {
+    span.textContent = contenido
+}
+
+function formatearNumero(numero) {
+    return '$' + numero.toFixed(2)
+}
+
+function mostrarBloque(bloqueTexto) {
+    bloqueTexto.classList.remove('hidden')
+    bloqueTexto.textContent = 'Ingresa un valor valido, no se permiten valores negativos'
+}
+
+function ocultarBloque(bloqueTexto) {
+    bloqueTexto.classList.add('hidden')
+}
+
+function calcularResultados(subtotal, porcentajePropina, numeroPersonas) {
+
+    const propina = subtotal * (porcentajePropina/100)
+    const total = subtotal + propina
+    const costoPorPersona = total / numeroPersonas
+
+    return{
+        propina,
+        costoPorPersona,
+        total
+    }
+}
+
+function reiniciarInterfaz() {
+    actualizarContenido(spanSubtotal, '$0.00')
+    actualizarContenido(spanResultadoPropina, '$0.00')
+    actualizarContenido(spanTotalPersonas, '$0.00')
+    actualizarContenido(spanResultadoTotal, '$0.00')
+}
+
+function mostrarResultados(valores, resultados) {
+    const { subtotal, porcentaje} = valores;
+    const { propina, costoPorPersona, total} = resultados;
+    actualizarContenido(spanSubtotal, formatearNumero(subtotal))
+    actualizarContenido(spanPropina, `Propina (${porcentaje}%)`)
+    actualizarContenido(spanResultadoPropina, formatearNumero(propina))
+    actualizarContenido(spanResultadoTotal, formatearNumero(total))
+    actualizarContenido(spanTotalPersonas, formatearNumero(costoPorPersona))
+}
+
+function validarDatos() {
+    
+}
+
+function calcularValores() { 
+    const valorSubtotal = convertirValoresDecimales(inputValorTotal)
+    const valorPorcentaje = convertirValoresEnteros(inputPorcentaje)
+    const valorPersonas = convertirValoresEnteros(inputCantidadPersonas)
+
+    const valores ={
+        subtotal: valorSubtotal,
+        porcentaje: valorPorcentaje,
+        personas: valorPersonas
+    }
+
+    if (isNaN(valores.subtotal) || isNaN(valores.porcentaje)|| isNaN(valores.personas)){
+        reiniciarInterfaz()
+        return
+    } else if ( valores.subtotal < 0 || valores.porcentaje < 0 || valores.personas <= 0){  
+        mostrarBloque(bloqueMensajeAlerta)
+        return
+    } else {
+        const resultados = calcularResultados(valores.subtotal, valores.porcentaje, valores.personas)
+        mostrarResultados(valores, resultados)
+        ocultarBloque(bloqueMensajeAlerta)
+        return
+    }
+}
+
+inputValorTotal.addEventListener('input', calcularValores)
+inputPorcentaje.addEventListener('input', calcularValores)
+inputCantidadPersonas.addEventListener('input', calcularValores)
